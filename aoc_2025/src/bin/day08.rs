6212e5c20d074b77f8d718b64fb2ef3db9b3ff31
Joshua Marsh (icub3d) <joshua.marshian@gmail.com>
2025-12-08 16:14:44 -0700

    2025 8

--- aoc_2025/src/bin/day08.rs
+++ aoc_2025/src/bin/day08.rs
@@ -1,17 +1,18 @@
 use std::time::Instant;
 
 use itertools::Itertools;
-use rustc_hash::FxHashMap;
+use rayon::prelude::*;
 
 const INPUT: &str = include_str!("inputs/day08.txt");
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Point {
     x: f64,
     y: f64,
     z: f64,
 }
 
+// Parse Point from string.
 impl From<&str> for Point {
     fn from(value: &str) -> Self {
         let mut pp = value.split(',').map(|v| v.parse::<f64>().unwrap());
@@ -24,12 +25,17 @@ impl From<&str> for Point {
 }
 
 impl Point {
+    // Calculate distance
+    // https://en.wikipedia.org/wiki/Euclidean_distance#Higher_dimensions
     fn distance(&self, rhs: &Point) -> f64 {
         let dx = rhs.x - self.x;
         let dy = rhs.y - self.y;
         let dz = rhs.z - self.z;
 
-        (dx.powi(2) + dy.powi(2) + dz.powi(2)).sqrt()
+        // The right distance includes sqrt(), we can (very slightly) optimize since we are only
+        // interested in order.
+        // (dx.powi(2) + dy.powi(2) + dz.powi(2)).sqrt()
+        dx.powi(2) + dy.powi(2) + dz.powi(2)
     }
 }
 
@@ -37,10 +43,12 @@ fn parse(input: &str) -> impl Iterator<Item = Point> {
     input.lines().map(Point::from)
 }
 
+// https://en.wikipedia.org/wiki/Disjoint_sets
 struct DisjointSet {
     parent: Vec<usize>,
+    // Track the size of each set (only parent is accurate for entire group).
     size: Vec<usize>,
-    // For p2, we'll want to track how many groups we have and stop when we have just one.
+    // Track the number of sets.
     count: usize,
 }
 
@@ -57,21 +65,19 @@ impl DisjointSet {
         if self.parent[i] == i {
             return i;
         }
-
         self.parent[i] = self.find(self.parent[i]);
         self.parent[i]
     }
 
+    // For p3, we are interested in the "last merge", so we return true if we made a group and then p3 can check if that merge was the last.
     fn union(&mut self, i: usize, j: usize) -> bool {
         let root_i = self.find(i);
         let root_j = self.find(j);
 
-        // If they were already connected, we are done.
         if root_i == root_j {
             return false;
         }
 
-        // Otherwise, we want to merge them and update our count.
         let (p1, p2) = match self.size[root_i] < self.size[root_j] {
             true => (root_i, root_j),
             false => (root_j, root_i),
@@ -83,49 +89,52 @@ impl DisjointSet {
     }
 }
 
-// BUG(FIXED): For the example input, we only want to make 10 connections.
+// BUG(FIXED): the test case sets a limit of 10, which was giving me the wrong answer.
 fn p1(input: &str, limit: usize) -> usize {
-    // Get our points.
     let points = parse(input).collect::<Vec<_>>();
 
-    // Calculate all distances.
-    let dists = points
+    let mut dists = points
         .iter()
         .enumerate()
         .flat_map(|(i1, p1)| {
             points[i1 + 1..]
                 .iter()
                 .enumerate()
-                // BUG(FIXED): i2 is in relation to i1+1.
+                // BUG(FIXED): i2 is from i1+1
                 .map(move |(i2, p2)| ((i1, i1 + 1 + i2), p1.distance(p2)))
         })
-        .sorted_unstable_by(|(_, d1), (_, d2)| d1.partial_cmp(d2).unwrap())
         .collect::<Vec<((usize, usize), f64)>>();
 
-    // Add limit pairs of them to our set.
+    // We don't have to completely sort, just make sure 0..limit are the smallest.
+    // "kth element" and then truncate (very minor optimization)
+    // https://en.wikipedia.org/wiki/Quickselect
+    // [ipnsort]: https://github.com/Voultapher/sort-research-rs/tree/main/ipnsort
+    // [total order]: https://en.wikipedia.org/wiki/Total_order
+    dists.select_nth_unstable_by(limit, |(_, d1), (_, d2)| d1.partial_cmp(d2).unwrap());
+    dists.truncate(limit);
+
+    // Add unions to our disjoint set.
     let mut ds = DisjointSet::new(points.len());
-    for ((i1, i2), _) in dists.into_iter().take(limit) {
+    for ((i1, i2), _) in dists {
         ds.union(i1, i2);
     }
 
-    // Make groupings of connected junction boxes.
-    let mut connected: FxHashMap<usize, usize> = FxHashMap::default();
-    for i in 0..points.len() {
-        let root = ds.find(i);
-        *connected.entry(root).or_default() += 1;
-    }
-
-    // Take the three largest and return their product.
-    connected
-        .values()
-        .sorted_unstable_by(|a, b| b.cmp(a))
+    // We know the sizes of each group, so we can simply find the parents and sort by their size and take the product of the largest 3.
+    ds.parent
+        .iter()
+        .enumerate()
+        .filter(|(i, p)| i == *p)
+        .map(|(i, _)| ds.size[i])
+        .sorted_unstable_by(|a, b| b.cmp(a)) // BUG(FIXED): largest
         .take(3)
         .product()
 }
 
+// p2 is largely the same as p1, we just track when all of the values have been sorted.
 fn p2(input: &str) -> usize {
     let points = parse(input).collect::<Vec<_>>();
-    let dists = points
+
+    let mut dists = points
         .iter()
         .enumerate()
         .flat_map(|(i1, p1)| {
@@ -134,14 +143,15 @@ fn p2(input: &str) -> usize {
                 .enumerate()
                 .map(move |(i2, p2)| ((i1, i1 + 1 + i2), p1.distance(p2)))
         })
-        .sorted_unstable_by(|(_, d1), (_, d2)| d1.partial_cmp(d2).unwrap())
         .collect::<Vec<((usize, usize), f64)>>();
 
-    // Union values from the set until we perform a merge that makes a single set.
+    // Sorting in parallel actually helps here.
+    dists.par_sort_unstable_by(|(_, d1), (_, d2)| d1.partial_cmp(d2).unwrap());
+
     let mut ds = DisjointSet::new(points.len());
     for ((i1, i2), _) in dists {
+        // Go until we find the last merge.
         if ds.union(i1, i2) && ds.count == 1 {
-            // Return the product of their x values.
             return (points[i1].x * points[i2].x) as usize;
         }
     }
